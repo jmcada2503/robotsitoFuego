2414cb90db4b0522a10f6097b4ade30faffcbb1c
Juan Miguel <juanmiscj25@gmail.com>
2023-04-04 22:52:24 -0500

    hotfix

--- motoresRobotFuego/motoresRobotFuego.ino
+++ motoresRobotFuego/motoresRobotFuego.ino
@@ -1,3 +1,55 @@
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
+#include <vector>
+
+Adafruit_AMG88xx amg;
+
+class Camera {
+
+    public:
+        bool reversed;
+
+        Motor(bool Reversed) {
+            reversed = Reversed;
+        }
+
+        void init() {
+            bool status;
+            satus = amg.begin();
+            if (!status) {
+                Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+                while (1);
+            }
+            delay(100);
+        }
+
+        vector<vector<float>> getPixelValues {
+            vector<vector<float>> values (AMG88xx_PIXEL_ARRAY_SIZE/8, vector<float>(AMG88xx_PIXEL_ARRAY_SIZE/8));
+
+            if (reversed) {
+                int row = 0;
+                for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
+                    values[row][i-((AMG88xx_PIXEL_ARRAY_SIZE/8)*row)] = pixels[AMG88xx_PIXEL_ARRAY_SIZE-1-i];
+                    if( i%8 == 0 ) row++;
+                }
+                return values;
+            }
+            else {
+                int row = 0;
+                for(int i=0; i<AMG88xx_PIXEL_ARRAY_SIZE; i++){
+                    values[row][i-((AMG88xx_PIXEL_ARRAY_SIZE/8)*row)] = pixels[i];
+                    if( i%8 == 0 ) row++;
+                }
+            }
+
+            return values;
+        }
+
+    private:
+        float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
+
+}
+
 class Motor {
 
     public:
@@ -42,11 +94,13 @@ class Motor {
 
 Motor rightMotor;
 Motor leftMotor;
+Camera camera(true);
 
 void setup(){
     Serial.begin(9600); 
     rightMotor.init(255, 11, 12, 13);
     leftMotor.init(200, 3, 2, 4);
+    camera.init();
 }
 
 void loop() {
